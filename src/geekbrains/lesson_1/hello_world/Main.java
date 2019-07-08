1669ed46aaa81ddd4e22d3e0cb3a8317c38f4432
Наше имя <>
2019-07-08 09:49:57 +0400

    Initial commit

---
+++ src/geekbrains/lesson_1/hello_world/Main.java
@@ -0,0 +1,12 @@
+package geekbrains.lesson_1.hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte a = 12;
+   a = (byte) (a + 1);
+   System.out.println(a);
+
+    }
+
+}
